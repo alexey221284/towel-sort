18a0ab833af1a12525c68a8f7556ca296963d57a
Alexey Zhumaev <alexey221284@mail.ru>
2020-03-22 00:08:03 +0400

    feat: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+module.exports = function towelSort(matrix) {
 
-// You should implement your task here.
+  if (!matrix || matrix.length <= 0) {
+    return []
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
+  const newArray1 = matrix.map((element, index) => {
+    if (index % 2) {
+      return element.sort((a, b) => b - a)
+    } else {
+      return element.sort((a, b) => a - b)
+    }
+  });
+
+  return newArray1.join(',').split(',')
 }
+
